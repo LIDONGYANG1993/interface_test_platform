06c67bbd8774fcfc3db7703d0e283a02cb901e20
lidongyang1993 <yang_yangwyu@hotmail.com>
2022-06-01 10:30:32 +0800

    环境文件； 配置

--- platformApp/settings.py
+++ platformApp/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = 'django-insecure-hbetbnyfrfe@*cyb^i5#m08sbpxcby6-_9b(c--3inju^neiu1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.17.32", "0.0.0.0", "1.116.254.250"]
+ALLOWED_HOSTS = ["192.168.17.32", "0.0.0.0", "1.116.254.250", "101.200.239.26"]
 
 # Application definition
 
