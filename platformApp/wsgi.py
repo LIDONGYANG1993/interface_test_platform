f706225bb32c29c715826590277f6f0444514706
lidongyang1993 <yang_yangwyu@hotmail.com>
2022-06-17 14:31:52 +0800

    v-0.8.1 增加部署文件

--- platformApp/wsgi.py
+++ platformApp/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'interface_test_platform.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'platformApp.settings')
 
 application = get_wsgi_application()
